4e63ca3af1bb06618f90dc896b3725eb75989c79
kaibz777 <kaibz777@gmail.com>
2024-08-20 01:19:52 +0200

    Fixed an issue where changing the weapon fov a weapon would influence others if they were not set to default (0)

--- DE/weaponFovManager.cpp
+++ DE/weaponFovManager.cpp
@@ -46,16 +46,16 @@ void weaponFovManager::update()
 	setCurWeaponFov(targetWeaponFov);
 }
 
-
+//! don't need this after all, changing fov often enough, is enough. beside lerping would look bad...
 float weaponFovManager::lerp(float a, float b, float t) {
 	return a + t * (b - a);
 }
 
 
 //? doesn't work it way too visible
-//void weaponFovManager::setCurWeaponFov(int weaponFov) {
+//void weaponFovManager::setCurWeaponFov(int desiredWeaponFov) {
 //	static float curWeaponFovF = fastCvarManager::getCurHands_fovScale();
-//	float targetWeaponFov = static_cast<float>(weaponFov);
+//	float targetWeaponFov = static_cast<float>(desiredWeaponFov);
 //
 //	// Calculate the interpolated FOV value
 //	float lerpSpeed = 0.9f; // Adjust this value to control the speed of the transition
@@ -63,31 +63,35 @@ float weaponFovManager::lerp(float a, float b, float t) {
 //
 //	// Update the FOV only if there is a noticeable difference
 //	if (fabs(curWeaponFovF - targetWeaponFov) > 0.01f) {
-//		logInfo("setCurWeaponFov: (int)curWeaponFovF is: %d updating it to: %d", static_cast<int>(curWeaponFovF), weaponFov);
+//		logInfo("setCurWeaponFov: (int)curWeaponFovF is: %d updating it to: %d", static_cast<int>(curWeaponFovF), desiredWeaponFov);
 //		idCmd::setHandsFov(static_cast<int>(curWeaponFovF));
 //	}
 //}
 
-
-void weaponFovManager::setCurWeaponFov(int weaponFov)
+//! this looks like a mess, but because weapon fov at 0 is not 0 but default fov, it makes everything a bit confusing.
+void weaponFovManager::setCurWeaponFov(int desiredWeaponFov)
 {
 	float curWeaponFovF = fastCvarManager::getCurHands_fovScale();
 
-	//logInfo("setCurWeaponFov: weaponFov: %d curWeaponFovF: %.3f", weaponFov, curWeaponFovF);
+	//logInfo("setCurWeaponFov: desiredWeaponFov: %d curWeaponFovF: %.3f", desiredWeaponFov, curWeaponFovF);
 
 	int curWeaponFovFormated = (int)(curWeaponFovF * 100);
 
-	//if (curWeaponFovF != (float)weaponFov) {
-	if (weaponFov > 0 && weaponFov < 10 && curWeaponFovFormated != 10) {
+	//if (curWeaponFovF != (float)desiredWeaponFov) {
+	if (desiredWeaponFov > 0 && desiredWeaponFov < 10 && curWeaponFovFormated != 10) {
 
 		//logInfo("setCurWeaponFov: forcing weapon fov to: 10 to prevent issues");
 		idCmd::setHandsFov(10);
 	}
 
-	else if ((weaponFov >= 10) && (curWeaponFovFormated != weaponFov)) {
+	else if ((desiredWeaponFov >= 10) && (curWeaponFovFormated != desiredWeaponFov)) {
 		
-		//logInfo("setCurWeaponFov: curWeaponFovFormated is: %d updating it to: %d", curWeaponFovFormated, weaponFov);
-		idCmd::setHandsFov(weaponFov);
+		//logInfo("setCurWeaponFov: curWeaponFovFormated is: %d updating it to: %d", curWeaponFovFormated, desiredWeaponFov);
+		idCmd::setHandsFov(desiredWeaponFov);
+	}
+
+	else if (desiredWeaponFov == 0 && curWeaponFovFormated != 0) {
+		idCmd::setHandsFov(desiredWeaponFov);
 	}
 }
 
